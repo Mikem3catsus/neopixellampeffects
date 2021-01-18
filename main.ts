b7a4ff7409aa6a25c2a2687b70360c46de28fdbb
Mikem3catsus <mikem@3cats.us>
2021-01-17 22:37:06 -0800

    Changed ratio of underlying tracking of strengths relative to the number of LEDs from 1:1 to 10:1

--- main.ts
+++ main.ts
@@ -4,19 +4,29 @@ let num_leds = 3
 let max_led_index = num_leds - 1
 let strip = neopixel.create(DigitalPin.P0, num_leds, NeoPixelMode.RGB)
 let wave_speed = 1
-let strengths = [num_leds]
+let wave_strength = 50
+let strengths = [num_leds*10]
 basic.forever(function () {
-    basic.pause(500)
-    for (let i = 0; i <= num_leds - 1; i++) {
+    basic.pause(100)
+    for (let i = 0; i <= num_leds*10 - 1; i++) {
         strengths[i] = 0
     }
     wave_location = wave_location + wave_speed
-    if (wave_location >= num_leds) {
+    if (wave_location >= num_leds*10) {
         wave_location = 0
+        wave_strength =  randint(10, 80)
+        wave_strength = 89
     }
-    strengths[wave_location] = 1
+    for(let i=0;i<10;i++){
+    strengths[wave_location+i] = wave_strength
+    }
     for (let j = 0; j <= num_leds - 1; j++) {
-        color = neopixel.hsl(240, 99, 10 + strengths[j] * 70)
+        let sum = 0
+        for(let k=0;k<10;k++){
+            sum = sum + strengths[j*10+k]
+        }
+        let lum = sum/10
+        color = neopixel.hsl(240-lum/5, 99-lum/2, 10 + lum)
         strip.setPixelColor(j, color)
     }
     //strip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
